cc63feb84ab1be1d5a2b447f30422bacb771a8d2
Oleksii Pavlenko <alexeypavlenko77@gmail.com>
2025-12-18 23:39:17 +0200

    Lesson 19 Operators

---
+++ lesson-19-js-operators/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const pricePerItem = 13500;
+let itemsCount = 3;
+let itemsTotalCount = 5;
+
+
+let exampleVariable = '33'
+
+itemsCount = itemsCount + 2;
+
+
+const isButtonDisabled = itemsCount <= itemsTotalCount;
+console.log(isButtonDisabled);
+
+console.log(   Number(exampleVariable) !== 33  )
+
+
+let isGenderFemale = true;
+let hasCookies = true;
+let canAccessAdminPanel = true;
+
+let rand = Math.random() * 100;
+console.log(Math.floor(rand));
+
+
+
+
+
+
